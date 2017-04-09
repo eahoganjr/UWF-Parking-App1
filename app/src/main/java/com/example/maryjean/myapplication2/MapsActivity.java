32922c4795360ff368ca56ca13257f474fc62f97
Jeff <monkeybuilderman@gmail.com>
2017-04-09 06:28:03 -0500

    Progress! Lots change colors dynamically! (Lot A does anyways)...

--- app/src/main/java/com/example/maryjean/myapplication2/MapsActivity.java
+++ app/src/main/java/com/example/maryjean/myapplication2/MapsActivity.java
@@ -3,6 +3,10 @@ package com.example.maryjean.myapplication2;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -37,7 +41,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private final int YELLOW = 85;
     private final int RED = 100;
 
-    public int lotastat = 90;
+    //private ArrayList<Integer> lotStats = new ArrayList<Integer>();
+
+    private int lotastat = 50;
+    private int lotbstat = 50;
+    private int lotcstat = 50;
+    private int lotestat = 50;
+    private int lotfstat = 50;
+    private int lotgstat = 50;
+    private int lothstat = 50;
+    private int lotistat = 50;
+    private int lotjstat = 50;
+    private int lotkstat = 50;
+    private int lotlstat = 50;
+    private int lotmstat = 50;
+    private int lotostat = 50;
+    private int lotpstat = 50;
+    private int lotqstat = 50;
+    private int lotrstat = 50;
+    private int lotsstat = 50;
+    private int lottstat = 50;
+    private int lotustat = 50;
+    private int lotvstat = 50;
+    private int lotwstat = 50;
+    private int lotxstat = 50;
+    private int lotystat = 50;
+    private int lotzstat = 50;
+    public String[] lotnames = {"lota.txt",
+            "lotb.txt",
+            "lotc.txt",
+            "lote.txt",
+            "lotf.txt",
+            "lotg.txt",
+            "loth.txt",
+            "loti.txt",
+            "lotj.txt",
+            "lotk.txt",
+            "lotl.txt",
+            "lotm.txt",
+            "loto.txt",
+            "lotp.txt",
+            "lotq.txt",
+            "lotr.txt",
+            "lots.txt",
+            "lott.txt",
+            "lotu.txt",
+            "lotv.txt",
+            "lotw.txt",
+            "lotx.txt",
+            "loty.txt",
+            "lotz.txt"
+    };
 
     Button buttonA, buttonB, buttonC, buttonE, buttonF, buttonG,
             buttonH, buttonI, buttonJ, buttonK, buttonL, buttonM,
@@ -133,7 +187,100 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             KmlLayer kmlLayeray = new KmlLayer(mMap, R.raw.lotay, getApplicationContext());
             KmlLayer kmlLayerar = new KmlLayer(mMap, R.raw.lotar, getApplicationContext());
 
-            KmlLayer kmlLayerb = new KmlLayer(mMap, R.raw.lotb, getApplicationContext());
+            KmlLayer kmlLayerbg = new KmlLayer(mMap, R.raw.lotbg, getApplicationContext());
+            KmlLayer kmlLayerby = new KmlLayer(mMap, R.raw.lotby, getApplicationContext());
+            KmlLayer kmlLayerbr = new KmlLayer(mMap, R.raw.lotbr, getApplicationContext());
+
+            KmlLayer kmlLayercg = new KmlLayer(mMap, R.raw.lotcg, getApplicationContext());
+            KmlLayer kmlLayercy = new KmlLayer(mMap, R.raw.lotcy, getApplicationContext());
+            KmlLayer kmlLayercr = new KmlLayer(mMap, R.raw.lotcr, getApplicationContext());
+
+            KmlLayer kmlLayereg = new KmlLayer(mMap, R.raw.loteg, getApplicationContext());
+            KmlLayer kmlLayerey = new KmlLayer(mMap, R.raw.lotey, getApplicationContext());
+            KmlLayer kmlLayerer = new KmlLayer(mMap, R.raw.loter, getApplicationContext());
+
+            KmlLayer kmlLayerfg = new KmlLayer(mMap, R.raw.lotfg, getApplicationContext());
+            KmlLayer kmlLayerfy = new KmlLayer(mMap, R.raw.lotfy, getApplicationContext());
+            KmlLayer kmlLayerfr = new KmlLayer(mMap, R.raw.lotfr, getApplicationContext());
+
+            KmlLayer kmlLayergg = new KmlLayer(mMap, R.raw.lotgg, getApplicationContext());
+            KmlLayer kmlLayergy = new KmlLayer(mMap, R.raw.lotgy, getApplicationContext());
+            KmlLayer kmlLayergr = new KmlLayer(mMap, R.raw.lotgr, getApplicationContext());
+
+            KmlLayer kmlLayerhg = new KmlLayer(mMap, R.raw.lothg, getApplicationContext());
+            KmlLayer kmlLayerhy = new KmlLayer(mMap, R.raw.lothy, getApplicationContext());
+            KmlLayer kmlLayerhr = new KmlLayer(mMap, R.raw.lothr, getApplicationContext());
+
+            KmlLayer kmlLayerig = new KmlLayer(mMap, R.raw.lotig, getApplicationContext());
+            KmlLayer kmlLayeriy = new KmlLayer(mMap, R.raw.lotiy, getApplicationContext());
+            KmlLayer kmlLayerir = new KmlLayer(mMap, R.raw.lotir, getApplicationContext());
+
+            KmlLayer kmlLayerjg = new KmlLayer(mMap, R.raw.lotjg, getApplicationContext());
+            KmlLayer kmlLayerjy = new KmlLayer(mMap, R.raw.lotjy, getApplicationContext());
+            KmlLayer kmlLayerjr = new KmlLayer(mMap, R.raw.lotjr, getApplicationContext());
+
+            KmlLayer kmlLayerkg = new KmlLayer(mMap, R.raw.lotkg, getApplicationContext());
+            KmlLayer kmlLayerky = new KmlLayer(mMap, R.raw.lotky, getApplicationContext());
+            KmlLayer kmlLayerkr = new KmlLayer(mMap, R.raw.lotkr, getApplicationContext());
+
+            KmlLayer kmlLayerlg = new KmlLayer(mMap, R.raw.lotlg, getApplicationContext());
+            KmlLayer kmlLayerly = new KmlLayer(mMap, R.raw.lotly, getApplicationContext());
+            KmlLayer kmlLayerlr = new KmlLayer(mMap, R.raw.lotlr, getApplicationContext());
+
+            KmlLayer kmlLayermg = new KmlLayer(mMap, R.raw.lotmg, getApplicationContext());
+            KmlLayer kmlLayermy = new KmlLayer(mMap, R.raw.lotmy, getApplicationContext());
+            KmlLayer kmlLayermr = new KmlLayer(mMap, R.raw.lotmr, getApplicationContext());
+
+            KmlLayer kmlLayerog = new KmlLayer(mMap, R.raw.lotog, getApplicationContext());
+            KmlLayer kmlLayeroy = new KmlLayer(mMap, R.raw.lotoy, getApplicationContext());
+            KmlLayer kmlLayeror = new KmlLayer(mMap, R.raw.lotor, getApplicationContext());
+
+            KmlLayer kmlLayerpg = new KmlLayer(mMap, R.raw.lotpg, getApplicationContext());
+            KmlLayer kmlLayerpy = new KmlLayer(mMap, R.raw.lotpy, getApplicationContext());
+            KmlLayer kmlLayerpr = new KmlLayer(mMap, R.raw.lotpr, getApplicationContext());
+
+            KmlLayer kmlLayerqg = new KmlLayer(mMap, R.raw.lotqg, getApplicationContext());
+            KmlLayer kmlLayerqy = new KmlLayer(mMap, R.raw.lotqy, getApplicationContext());
+            KmlLayer kmlLayerqr = new KmlLayer(mMap, R.raw.lotqr, getApplicationContext());
+
+            KmlLayer kmlLayerrg = new KmlLayer(mMap, R.raw.lotrg, getApplicationContext());
+            KmlLayer kmlLayerry = new KmlLayer(mMap, R.raw.lotry, getApplicationContext());
+            KmlLayer kmlLayerrr = new KmlLayer(mMap, R.raw.lotrr, getApplicationContext());
+
+            KmlLayer kmlLayersg = new KmlLayer(mMap, R.raw.lotsg, getApplicationContext());
+            KmlLayer kmlLayersy = new KmlLayer(mMap, R.raw.lotsy, getApplicationContext());
+            KmlLayer kmlLayersr = new KmlLayer(mMap, R.raw.lotsr, getApplicationContext());
+
+            KmlLayer kmlLayertg = new KmlLayer(mMap, R.raw.lottg, getApplicationContext());
+            KmlLayer kmlLayerty = new KmlLayer(mMap, R.raw.lotty, getApplicationContext());
+            KmlLayer kmlLayertr = new KmlLayer(mMap, R.raw.lottr, getApplicationContext());
+
+            KmlLayer kmlLayerug = new KmlLayer(mMap, R.raw.lotug, getApplicationContext());
+            KmlLayer kmlLayeruy = new KmlLayer(mMap, R.raw.lotuy, getApplicationContext());
+            KmlLayer kmlLayerur = new KmlLayer(mMap, R.raw.lotur, getApplicationContext());
+
+            KmlLayer kmlLayervg = new KmlLayer(mMap, R.raw.lotvg, getApplicationContext());
+            KmlLayer kmlLayervy = new KmlLayer(mMap, R.raw.lotvy, getApplicationContext());
+            KmlLayer kmlLayervr = new KmlLayer(mMap, R.raw.lotvr, getApplicationContext());
+
+            KmlLayer kmlLayerwg = new KmlLayer(mMap, R.raw.lotwg, getApplicationContext());
+            KmlLayer kmlLayerwy = new KmlLayer(mMap, R.raw.lotwy, getApplicationContext());
+            KmlLayer kmlLayerwr = new KmlLayer(mMap, R.raw.lotwr, getApplicationContext());
+
+            KmlLayer kmlLayerxg = new KmlLayer(mMap, R.raw.lotxg, getApplicationContext());
+            KmlLayer kmlLayerxy = new KmlLayer(mMap, R.raw.lotxy, getApplicationContext());
+            KmlLayer kmlLayerxr = new KmlLayer(mMap, R.raw.lotxr, getApplicationContext());
+
+            KmlLayer kmlLayeryg = new KmlLayer(mMap, R.raw.lotyg, getApplicationContext());
+            KmlLayer kmlLayeryy = new KmlLayer(mMap, R.raw.lotyy, getApplicationContext());
+            KmlLayer kmlLayeryr = new KmlLayer(mMap, R.raw.lotyr, getApplicationContext());
+
+            KmlLayer kmlLayerzg = new KmlLayer(mMap, R.raw.lotzg, getApplicationContext());
+            KmlLayer kmlLayerzy = new KmlLayer(mMap, R.raw.lotzy, getApplicationContext());
+            KmlLayer kmlLayerzr = new KmlLayer(mMap, R.raw.lotzr, getApplicationContext());
+
+
+            /*KmlLayer kmlLayerb = new KmlLayer(mMap, R.raw.lotb, getApplicationContext());
             KmlLayer kmlLayerc = new KmlLayer(mMap, R.raw.lotc, getApplicationContext());
             KmlLayer kmlLayere = new KmlLayer(mMap, R.raw.lote, getApplicationContext());
             KmlLayer kmlLayerf = new KmlLayer(mMap, R.raw.lotf, getApplicationContext());
@@ -155,9 +302,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             KmlLayer kmlLayerw = new KmlLayer(mMap, R.raw.lotw, getApplicationContext());
             KmlLayer kmlLayerx = new KmlLayer(mMap, R.raw.lotx, getApplicationContext());
             KmlLayer kmlLayery = new KmlLayer(mMap, R.raw.loty, getApplicationContext());
-            KmlLayer kmlLayerz = new KmlLayer(mMap, R.raw.lotz, getApplicationContext());
+            KmlLayer kmlLayerz = new KmlLayer(mMap, R.raw.lotz, getApplicationContext()); */
 
             kmlLayer.addLayerToMap();
+
+            readPercents();
 
             //kmlLayera.addLayerToMap();
             if(MIN <= lotastat && lotastat < GREEN)
@@ -167,8 +316,171 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             else if(YELLOW <= lotastat && lotastat <= RED)
                 kmlLayerar.addLayerToMap();
 
+            if(MIN <= lotbstat && lotbstat < GREEN)
+                kmlLayerbg.addLayerToMap();
+            else if(GREEN <= lotbstat && lotbstat < YELLOW)
+                kmlLayerby.addLayerToMap();
+            else if(YELLOW <= lotbstat && lotbstat <= RED)
+                kmlLayerbr.addLayerToMap();
 
-            kmlLayerb.addLayerToMap();
+            if(MIN <= lotcstat && lotcstat < GREEN)
+                kmlLayercg.addLayerToMap();
+            else if(GREEN <= lotcstat && lotcstat < YELLOW)
+                kmlLayercy.addLayerToMap();
+            else if(YELLOW <= lotcstat && lotcstat <= RED)
+                kmlLayercr.addLayerToMap();
+
+            if(MIN <= lotestat && lotestat < GREEN)
+                kmlLayereg.addLayerToMap();
+            else if(GREEN <= lotestat && lotestat < YELLOW)
+                kmlLayerey.addLayerToMap();
+            else if(YELLOW <= lotestat && lotestat <= RED)
+                kmlLayerer.addLayerToMap();
+
+            if(MIN <= lotfstat && lotfstat < GREEN)
+                kmlLayerfg.addLayerToMap();
+            else if(GREEN <= lotfstat && lotfstat < YELLOW)
+                kmlLayerfy.addLayerToMap();
+            else if(YELLOW <= lotfstat && lotfstat <= RED)
+                kmlLayerfr.addLayerToMap();
+
+            if(MIN <= lotgstat && lotgstat < GREEN)
+                kmlLayergg.addLayerToMap();
+            else if(GREEN <= lotgstat && lotgstat < YELLOW)
+                kmlLayergy.addLayerToMap();
+            else if(YELLOW <= lotgstat && lotgstat <= RED)
+                kmlLayergr.addLayerToMap();
+
+            if(MIN <= lothstat && lothstat < GREEN)
+                kmlLayerhg.addLayerToMap();
+            else if(GREEN <= lothstat && lothstat < YELLOW)
+                kmlLayerhy.addLayerToMap();
+            else if(YELLOW <= lothstat && lothstat <= RED)
+                kmlLayerhr.addLayerToMap();
+
+            if(MIN <= lotistat && lotistat < GREEN)
+                kmlLayerig.addLayerToMap();
+            else if(GREEN <= lotistat && lotistat < YELLOW)
+                kmlLayeriy.addLayerToMap();
+            else if(YELLOW <= lotistat && lotistat <= RED)
+                kmlLayerir.addLayerToMap();
+
+            if(MIN <= lotjstat && lotjstat < GREEN)
+                kmlLayerjg.addLayerToMap();
+            else if(GREEN <= lotjstat && lotjstat < YELLOW)
+                kmlLayerjy.addLayerToMap();
+            else if(YELLOW <= lotjstat && lotjstat <= RED)
+                kmlLayerjr.addLayerToMap();
+
+            if(MIN <= lotkstat && lotkstat < GREEN)
+                kmlLayerkg.addLayerToMap();
+            else if(GREEN <= lotkstat && lotkstat < YELLOW)
+                kmlLayerky.addLayerToMap();
+            else if(YELLOW <= lotkstat && lotkstat <= RED)
+                kmlLayerkr.addLayerToMap();
+
+            if(MIN <= lotlstat && lotlstat < GREEN)
+                kmlLayerlg.addLayerToMap();
+            else if(GREEN <= lotlstat && lotlstat < YELLOW)
+                kmlLayerly.addLayerToMap();
+            else if(YELLOW <= lotlstat && lotlstat <= RED)
+                kmlLayerlr.addLayerToMap();
+
+            if(MIN <= lotmstat && lotmstat < GREEN)
+                kmlLayermg.addLayerToMap();
+            else if(GREEN <= lotmstat && lotmstat < YELLOW)
+                kmlLayermy.addLayerToMap();
+            else if(YELLOW <= lotmstat && lotmstat <= RED)
+                kmlLayermr.addLayerToMap();
+
+            if(MIN <= lotostat && lotostat < GREEN)
+                kmlLayerog.addLayerToMap();
+            else if(GREEN <= lotostat && lotostat < YELLOW)
+                kmlLayeroy.addLayerToMap();
+            else if(YELLOW <= lotostat && lotostat <= RED)
+                kmlLayeror.addLayerToMap();
+
+            if(MIN <= lotpstat && lotpstat < GREEN)
+                kmlLayerpg.addLayerToMap();
+            else if(GREEN <= lotpstat && lotpstat < YELLOW)
+                kmlLayerpy.addLayerToMap();
+            else if(YELLOW <= lotpstat && lotpstat <= RED)
+                kmlLayerpr.addLayerToMap();
+
+            if(MIN <= lotwstat && lotwstat < GREEN)
+                kmlLayerqg.addLayerToMap();
+            else if(GREEN <= lotwstat && lotwstat < YELLOW)
+                kmlLayerqy.addLayerToMap();
+            else if(YELLOW <= lotwstat && lotwstat <= RED)
+                kmlLayerqr.addLayerToMap();
+
+            if(MIN <= lotxstat && lotxstat < GREEN)
+                kmlLayerrg.addLayerToMap();
+            else if(GREEN <= lotxstat && lotxstat < YELLOW)
+                kmlLayerry.addLayerToMap();
+            else if(YELLOW <= lotxstat && lotxstat <= RED)
+                kmlLayerrr.addLayerToMap();
+
+            if(MIN <= lotystat && lotystat < GREEN)
+                kmlLayersg.addLayerToMap();
+            else if(GREEN <= lotystat && lotystat < YELLOW)
+                kmlLayersy.addLayerToMap();
+            else if(YELLOW <= lotystat && lotystat <= RED)
+                kmlLayersr.addLayerToMap();
+
+            if(MIN <= lotzstat && lotzstat < GREEN)
+                kmlLayertg.addLayerToMap();
+            else if(GREEN <= lotzstat && lotzstat < YELLOW)
+                kmlLayerty.addLayerToMap();
+            else if(YELLOW <= lotzstat && lotzstat <= RED)
+                kmlLayertr.addLayerToMap();
+
+            if(MIN <= lotastat && lotastat < GREEN)
+                kmlLayerug.addLayerToMap();
+            else if(GREEN <= lotastat && lotastat < YELLOW)
+                kmlLayeruy.addLayerToMap();
+            else if(YELLOW <= lotastat && lotastat <= RED)
+                kmlLayerur.addLayerToMap();
+
+            if(MIN <= lotastat && lotastat < GREEN)
+                kmlLayervg.addLayerToMap();
+            else if(GREEN <= lotastat && lotastat < YELLOW)
+                kmlLayervy.addLayerToMap();
+            else if(YELLOW <= lotastat && lotastat <= RED)
+                kmlLayervr.addLayerToMap();
+
+            if(MIN <= lotastat && lotastat < GREEN)
+                kmlLayerwg.addLayerToMap();
+            else if(GREEN <= lotastat && lotastat < YELLOW)
+                kmlLayerwy.addLayerToMap();
+            else if(YELLOW <= lotastat && lotastat <= RED)
+                kmlLayerwr.addLayerToMap();
+
+            if(MIN <= lotastat && lotastat < GREEN)
+                kmlLayerxg.addLayerToMap();
+            else if(GREEN <= lotastat && lotastat < YELLOW)
+                kmlLayerxy.addLayerToMap();
+            else if(YELLOW <= lotastat && lotastat <= RED)
+                kmlLayerxr.addLayerToMap();
+
+            if(MIN <= lotastat && lotastat < GREEN)
+                kmlLayeryg.addLayerToMap();
+            else if(GREEN <= lotastat && lotastat < YELLOW)
+                kmlLayeryy.addLayerToMap();
+            else if(YELLOW <= lotastat && lotastat <= RED)
+                kmlLayeryr.addLayerToMap();
+
+            if(MIN <= lotzstat && lotzstat < GREEN)
+                kmlLayerzg.addLayerToMap();
+            else if(GREEN <= lotzstat && lotzstat < YELLOW)
+                kmlLayerzy.addLayerToMap();
+            else if(YELLOW <= lotzstat && lotzstat <= RED)
+                kmlLayerzr.addLayerToMap();
+
+
+
+
+            /* kmlLayerb.addLayerToMap();
             kmlLayerc.addLayerToMap();
             kmlLayere.addLayerToMap();
             kmlLayerf.addLayerToMap();
@@ -190,7 +502,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             kmlLayerw.addLayerToMap();
             kmlLayerx.addLayerToMap();
             kmlLayery.addLayerToMap();
-            kmlLayerz.addLayerToMap();
+            kmlLayerz.addLayerToMap(); */
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -233,7 +545,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+    public void readPercents() {
+        ArrayList<Integer> lotnameslist = new ArrayList<Integer>();
+        for(int i=0; i<lotnames.length; i++) {
+            Scanner reader;
+            try {
+                reader = new Scanner(new FileInputStream(lotnames[i]));
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            int j = reader.nextInt();
+            reader.close();
+            lotnameslist.add(j);
 
+        }
+        lotastat = lotnameslist.remove(0);
+        lotbstat = lotnameslist.remove(0);
+        lotcstat = lotnameslist.remove(0);
+        lotestat = lotnameslist.remove(0);
+        lotfstat = lotnameslist.remove(0);
+        lotgstat = lotnameslist.remove(0);
+        lothstat = lotnameslist.remove(0);
+        lotistat = lotnameslist.remove(0);
+        lotjstat = lotnameslist.remove(0);
+        lotkstat = lotnameslist.remove(0);
+        lotlstat = lotnameslist.remove(0);
+        lotmstat = lotnameslist.remove(0);
+        lotostat = lotnameslist.remove(0);
+        lotpstat = lotnameslist.remove(0);
+        lotqstat = lotnameslist.remove(0);
+        lotrstat = lotnameslist.remove(0);
+        lotsstat = lotnameslist.remove(0);
+        lottstat = lotnameslist.remove(0);
+        lotustat = lotnameslist.remove(0);
+        lotvstat = lotnameslist.remove(0);
+        lotwstat = lotnameslist.remove(0);
+        lotxstat = lotnameslist.remove(0);
+        lotystat = lotnameslist.remove(0);
+        lotzstat = lotnameslist.remove(0);
+    }
 
 
 
